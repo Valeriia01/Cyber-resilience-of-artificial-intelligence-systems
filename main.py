e29542f054ab29518d86e027361c68de5000039c
Valeriia01 <57660675+Valeriia01@users.noreply.github.com>
2023-10-25 12:12:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ import os
 from fastapi import FastAPI, status
 import redis
 import uvicorn
-from dotenv import dotenv_values, load_dotenv
+from dotenv import dotenv_values
 
-
+logger = logging.getLogger('informing')
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler('site_informing.log')
+fh.setLevel(logging.INFO)
+formatstr = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+formatter = logging.Formatter(formatstr)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.info('RESTARTED')
 
 config = {
     **dotenv_values(".env"),  # load sensitive variables
@@ -21,7 +29,7 @@ app = FastAPI()
 @app.get('/show/{web_host}')
 def show(web_host):
     """function to display the number of visits"""
-    logger.info(f'Displaying info about {web_host}') 
+    logger.info('Displaying info about %s' %web_host) 
     return {conn.get(web_host)}
 
 
@@ -29,19 +37,10 @@ def show(web_host):
 def visit(web_host):
     """increment function hkjsfadhjsdhjsdhj"""
     conn.incr(web_host)
-    logger.info(f'Visiting {web_host}')
+    logger.info('Visiting %s' %web_host)
     return status.HTTP_200_OK
 
 
 if __name__ == "__main__":
-   logger = logging.getLogger('informing')
-   logger.setLevel(logging.INFO)
-   fh = logging.FileHandler('site_informing.log')
-   fh.setLevel(logging.INFO)
-   formatstr = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-   formatter = logging.Formatter(formatstr)
-   fh.setFormatter(formatter)
-   logger.addHandler(fh)
-   logger.info('RESTARTED')
-   uvicorn.run(app,host = config["host_app"], port = int(config['site_port']),
+    uvicorn.run(app,host = config["host_app"], port = int(config['site_port']),
                 log_level = config['log_level'])
